4d4a501e656b5731bbf0085487f727a4b7c3708f
moporgic <hguei.cs03g@g2.nctu.edu.tw>
2017-05-24 18:21:51 +0800

    add unit test for CardManager (with a basic instance check)

---
+++ HearthStone/HearthStone.Library.Test/CardManagerUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthStone.Library.Test
+{
+    [TestClass]
+    class CardManagerUnitTest
+    {
+
+        [TestMethod]
+        public void StaticInstanceTestMethod1()
+        {
+            CardManager instance = CardManager.Instance;
+            Assert.IsNotNull(instance);
+        }
+
+    }
+}
