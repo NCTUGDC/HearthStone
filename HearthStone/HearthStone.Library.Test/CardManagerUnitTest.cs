db60d300431519cd937978a1bc21429f56b8e734
moporgic <hguei.cs03g@g2.nctu.edu.tw>
2017-05-24 18:23:12 +0800

    add 2 test cases for IEnumerable<Card> Cards

--- HearthStone/HearthStone.Library.Test/CardManagerUnitTest.cs
+++ HearthStone/HearthStone.Library.Test/CardManagerUnitTest.cs
@@ -18,5 +18,25 @@ namespace HearthStone.Library.Test
             Assert.IsNotNull(instance);
         }
 
+        [TestMethod]
+        public void CardsEnumerableTestMethod1()
+        {
+            IEnumerable<Card> cards = CardManager.Instance.Cards;
+            Assert.IsNotNull(cards);
+        }
+
+
+        [TestMethod]
+        public void CardsEnumerableTestMethod2()
+        {
+            HashSet<int> IDs = new HashSet<int>();
+            foreach (Card card in CardManager.Instance.Cards)
+            {
+                Assert.IsNotNull(card);
+                Assert.IsFalse(IDs.Contains(card.CardID));
+                IDs.Add(card.CardID);
+            }
+        }
+
     }
 }
