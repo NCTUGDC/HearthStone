a9baa9c5d60f720c01e865614ce63f8f80596e89
moporgic <hguei.cs03g@g2.nctu.edu.tw>
2017-05-24 19:53:50 +0800

    add basic structure for CardRecordUnitTest

---
+++ HearthStone/HearthStone.Library.Test/CardRecordUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthStone.Library.Test
+{
+    [TestClass]
+    public class CardRecordUnitTest
+    {
+        class TestCardRecord : CardRecord
+        {
+            TestCardRecord(int cardRecordID, int cardID) : base(cardRecordID, cardID) { }
+
+            void setCardRecordID(int ID)
+            {
+                CardRecordID = ID;
+            }
+        }
+
+    }
+}
