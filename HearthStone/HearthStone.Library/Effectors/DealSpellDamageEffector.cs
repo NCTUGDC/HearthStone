bcf236b4c28cf1e0f68553bdb0f25c76ea80bd64
DSobscure <snbruce13@yahoo.com.tw>
2017-06-04 22:02:17 +0800

    TestPass：Episode_CastSpell_AOE1_TestMethod1
    修正有造成法術傷害效果的effector 當未裝備武器時也能正確運作

--- HearthStone/HearthStone.Library/Effectors/DealSpellDamageEffector.cs
+++ HearthStone/HearthStone.Library/Effectors/DealSpellDamageEffector.cs
@@ -26,7 +26,7 @@ namespace HearthStone.Library.Effectors
             if (effect != null && effect is DealSpellDamageEffect)
             {
                 int extraSpellDamage1 = user.Game.SelfField(user.GamePlayerID).Cards(user.Game.GameCardManager).Sum(x => x.Effectors(user.Game.GameCardManager).OfType<SpellDamageEffector>().Sum(y => y.SpellDamage()));
-                int extraSpellDamage2 = user.Hero.Weapon(user.Game.GameCardManager).Effectors(user.Game.GameCardManager).OfType<SpellDamageEffector>().Sum(y => y.SpellDamage());
+                int extraSpellDamage2 = (user.Hero.WeaponCardRecordID != 0) ? user.Hero.Weapon(user.Game.GameCardManager).Effectors(user.Game.GameCardManager).OfType<SpellDamageEffector>().Sum(y => y.SpellDamage()) : 0;
                 servant.RemainedHealth -= (effect as DealSpellDamageEffect).Damage + extraSpellDamage1 + extraSpellDamage2;
             }
         }
--- HearthStone/HearthStone.Library/Effectors/DealSpellDamageToAllEnemyMinionsEffector.cs
+++ HearthStone/HearthStone.Library/Effectors/DealSpellDamageToAllEnemyMinionsEffector.cs
@@ -16,7 +16,7 @@ namespace HearthStone.Library.Effectors
             if (effect != null && effect is DealSpellDamageToAllEnemyMinionsEffect)
             {
                 int extraSpellDamage1 = user.Game.SelfField(user.GamePlayerID).Cards(user.Game.GameCardManager).Sum(x => x.Effectors(user.Game.GameCardManager).OfType<SpellDamageEffector>().Sum(y => y.SpellDamage()));
-                int extraSpellDamage2 = user.Hero.Weapon(user.Game.GameCardManager).Effectors(user.Game.GameCardManager).OfType<SpellDamageEffector>().Sum(y => y.SpellDamage());
+                int extraSpellDamage2 = (user.Hero.WeaponCardRecordID != 0) ? user.Hero.Weapon(user.Game.GameCardManager).Effectors(user.Game.GameCardManager).OfType<SpellDamageEffector>().Sum(y => y.SpellDamage()) : 0;
                 int damage = (effect as DealSpellDamageToAllEnemyMinionsEffect).Damage + extraSpellDamage1 + extraSpellDamage2;
                 foreach(var fieldCard in user.Game.OpponentField(user.GamePlayerID).FieldCards)
                 {
